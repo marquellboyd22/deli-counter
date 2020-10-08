f91820615f0a50227b41e4806775925fde6aa18b
Marquell boyd <marquellboyd@icloud.com>
2020-10-07 23:35:36 -0400

    Update deli_counter.rb

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+def line (deli)
+    if deli.empty?
+        puts "there is nobody in line ".
+    else
+        current_line = "there is nobody in line ".
+        deli.each.with.index do| person |
+            current_line 
+        end
+        def take_a_number  (deli , name )
+            deli << name
